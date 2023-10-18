462bbf69903c774ed84670bb381263e768a8538e
RegmiS <sandeshreg35@gmail.com>
2023-10-17 21:05:48 -0500

    Fix html, setup JS file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const add = (a, b) => {
+	return a+b;
+};
+
+const subtract = (a,b) => {
+	return a-b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (arr) => {
+    return a/b;
+};
+
+const calcText = document.querySelector('.text-display');
+
+const valOne =  document.getElementById('val-1');
+valOne.onclick = () => {
+    
+};
